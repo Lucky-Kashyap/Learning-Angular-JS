efc297c96a828d5d25e1c4ed3fb9590ad988d8ad
Lucky-Kashyap <luckykashyap3430@gmail.com>
2024-01-18 16:54:55 +0530

    use input for data sharing

--- blog/src/app/housing-location/housing-location.component.ts
+++ blog/src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
