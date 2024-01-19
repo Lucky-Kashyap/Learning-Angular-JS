e3f78a603888b8e25bf35e53324fce0d97d9147e
Lucky-Kashyap <luckykashyap3430@gmail.com>
2024-01-19 16:03:15 +0530

    getting error in imports standalone

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  // standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [HomeComponent,RouterModule],
   styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'blog';
--- blog/src/main.ts
+++ blog/src/main.ts
@@ -1,12 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import {provideProtractorTestingSupport} from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  })
   .catch(err => console.error(err));
+
+  // bootstrapApplication(AppComponent,
+    
+  // ).catch(err => console.error(err));
